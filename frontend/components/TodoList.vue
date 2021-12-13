70f377fc309bb0027d7fedf505d5d9e60c86fbce
Kyohei Toyoda <kyohei.toyoda@crowdworks.co.jp>
2021-12-13 18:09:32 +0900

    add template todo

---
+++ frontend/components/TodoList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>未/済</th>
+          <th>名前</th>
+          <th>詳細</th>
+          <th>編集</th>
+          <th>削除</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="todo in todos" v-bind:key="todo.id">
+          <td>
+            <template v-if="todo.completed">済</template>
+            <template v-else>未</template>
+          </td>
+          <td>{{ todo.name }}</td>
+          <td>{{ todo.detail }}</td>
+          <td><button>編集</button></td>
+          <td>
+            <template v-if="!todo.deleted"><button>削除</button></template>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button>追加</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { reactive } from 'vue';
+
+export default Vue.defineComponent({
+  setup() {
+    const todos = reactive([
+      {
+        id: 1,
+        name: 'hoge',
+        detail: 'this is detail',
+        completed: false,
+        deleted: false,
+      },
+    ]);
+
+    return { todos };
+  },
+});
+</script>
+
+<style scoped>
+table,
+td {
+  border: 1px solid #333;
+}
+
+thead,
+tfoot {
+  background-color: rgb(116, 182, 237);
+  color: rgb(255, 255, 255);
+}
+</style>
---
+++ frontend/pages/todo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <TodoList />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TodoList from '/components/TodoList.vue'
+
+export default Vue.defineComponent({
+  components: {
+    TodoList
+  }
+})
+</script>
+
+<style scoped></style>
