3e59596e05304e62c11ffb7bf453e68ea56989ce
Kyohei Toyoda <kyohei.toyoda@crowdworks.co.jp>
2021-12-28 18:32:55 +0900

    add put api schema

--- frontend/lib/api/openapi/services/ServiceService.ts
+++ frontend/lib/api/openapi/services/ServiceService.ts
@@ -1,6 +1,7 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import type { Todo } from '../models/Todo';
 import type { Todos } from '../models/Todos';
 import type { CancelablePromise } from '../core/CancelablePromise';
 import { request as __request } from '../core/request';
@@ -10,12 +11,39 @@ export class ServiceService {
     /**
      * TODOをすべて取得する
      * @returns Todos TODOの配列
+     * @returns any Created
      * @throws ApiError
      */
-    public static getTodos(): CancelablePromise<Todos> {
+    public static getTodos(): CancelablePromise<Todos | any> {
         return __request({
             method: 'GET',
             path: `/apiv3/todos`,
+        });
+    }
+
+    /**
+     * @param todoId todoのid
+     * @param requestBody
+     * @returns Todo 単体のTodo
+     * @throws ApiError
+     */
+    public static putTodo(
+        todoId: number,
+        requestBody?: {
+            todo: {
+                name: string;
+                detail: string;
+            };
+        },
+    ): CancelablePromise<Todo> {
+        return __request({
+            method: 'PUT',
+            path: `/apiv3/todos/${todoId}`,
+            body: requestBody,
+            mediaType: 'application/json',
+            errors: {
+                400: `Bad Request`,
+            },
         });
     }
 
